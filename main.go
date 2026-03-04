95a82ced875cddb60db434b03bafb619d4961eae
Ubuntu Server from home <zemsckov.dmitry@yandex.ru>
2026-03-05 00:30:14 +0300

    Complete main code. Test remain

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"slices"
+	"sync"
 	"time"
 )
 
@@ -21,7 +23,7 @@ func generateRandomElements(size int) []int {
 
 	randomElements := make([]int, size)
 	for i := 0; i < size; i++ {
-		randomElements = append(randomElements, int(src.Int63()))
+		randomElements[i] = int(src.Int63())
 	}
 
 	return randomElements
@@ -30,24 +32,73 @@ func generateRandomElements(size int) []int {
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	return slices.Max(data)
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	var wg sync.WaitGroup
+
+	// делим слайс на 8 частей
+	chunkSize := len(data) / CHUNKS
+	chunk := CHUNKS
+	if chunkSize == 0 {
+		chunkSize = 1
+		chunk = len(data)
+	}
+
+	// сохраняем все 8 максимальных значений в другом новом слайсе
+	maxValuesFromChunk := make([]int, chunk)
+
+	wg.Add(chunk)
+	for i := 1; i <= chunk; i++ {
+		// создайте переменную типа sync.WaitGroup и используйте её при запуске и ожидании горутин
+
+		var clip []int
+		switch i {
+		case chunk:
+			clip = data[chunkSize*(i-1):]
+		default:
+			clip = data[chunkSize*(i-1) : chunkSize*i]
+		}
+
+		go func(idx int, newClip []int) {
+			defer wg.Done()
+			maxFromChunk := slices.Max(newClip)
+			maxValuesFromChunk[idx-1] = maxFromChunk
+		}(i, clip)
+	}
+
+	wg.Wait()
+	// находим среди 8-ми максимальных значений максимум
+	return slices.Max(maxValuesFromChunk)
 }
 
 func main() {
 	fmt.Printf("Генерируем %d целых чисел", SIZE)
 	// ваш код здесь
+	randomElements := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
+	start := time.Now()
+	max := maximum(randomElements)
+	elapsed := time.Since(start).Milliseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
 	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
 	// ваш код здесь
+	start = time.Now()
+	max = maxChunks(randomElements)
+	elapsed = time.Since(start).Milliseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -6,13 +6,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var randomElements []int
+
 // Пишите тесты в этом файле
 func TestGenerateRandomElements(t *testing.T) {
-	var randomElements []int = generateRandomElements(0)
+	randomElements = generateRandomElements(0)
 	require.Nil(t, randomElements)
 
 	var sumElements int = 12345
 	randomElements = generateRandomElements(sumElements)
 	require.NotNil(t, randomElements)
 	require.Len(t, len(randomElements), sumElements)
+}
+
+func TestMaximum(t *testing.T) {
+	randomElements = make([]int, 0)
+	maxValue := maximum(randomElements)
+	require.Zero(t, maxValue)
+
+	randomElements = make([]int, 1)
+	maxNum := 50
+	randomElements[0] = maxNum
+	maxValue = maximum(randomElements)
+	require.Equal(t, maxNum, maxValue)
+
+	randomElements = make([]int, 5)
+	randomElements = []int{10, -50, 0, 803040, 100}
+	maxValue = maximum(randomElements)
+	require.Equal(t, 803040, maxValue)
+
+	randomElements = []int{0, 0, 0, 0, 0}
+	maxValue = maximum(randomElements)
+	require.Equal(t, 0, maxValue)
+
+	randomElements = []int{40, 40, 40, 40, 40}
+	maxValue = maximum(randomElements)
+	require.Equal(t, 40, maxValue)
+
+	randomElements = []int{-200, -200, -200, -200, -200}
+	maxValue = maximum(randomElements)
+	require.Equal(t, -200, maxValue)
 }
